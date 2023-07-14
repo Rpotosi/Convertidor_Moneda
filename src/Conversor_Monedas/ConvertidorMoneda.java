5f969fca948fc3dc61abca5031164d035a53c46e
Rpotosi <rpotosi@estudiantes.areandina.edu.co>
2023-07-13 23:45:50 -0500

    commit 2

--- src/Conversor_Monedas/ConvertidorMoneda.java
+++ src/Conversor_Monedas/ConvertidorMoneda.java
@@ -5,32 +5,57 @@ public class ConvertidorMoneda { //Clase
 	
 	//Atributos
 	
-	double tipoCambioDolar = 4.190;  // Tipo de cambio de tu país a Dólar
+	double tipoCambioDolar = 4.1966;  // Tipo de cambio de tu país a Dólar
     double tipoCambioEuro = 4.601;  // Tipo de cambio de tu país a Euro
-    double tipoCambioLibra = 0.00019;  // Tipo de cambio de tu país a Libra Esterlina
-    double tipoCambioYen = 0.034;  // Tipo de cambio de tu país a Yen Japonés
-    double tipoCambioWon = 0.31;  // Tipo de cambio de tu país a Won sur-coreano
+    double tipoCambioLibra = 5.385;  // Tipo de cambio de tu país a Libra Esterlina
+    double tipoCambioYen = 29.82;  // Tipo de cambio de tu país a Yen Japonés
+    double tipoCambioWon = 3.25;  // Tipo de cambio de tu país a Won sur-coreano
     
+    double tipoCambioPesoD = 0.00024;
+    double tipoCambioPesoE = 0.00022;
+    double tipoCambioPesoL = 0.00019;
+    double tipoCambioPesoY = 0.033;
+    double tipoCambioPesoW = 0.31;// Tipo de cambio de tu país a Dólar
+ 
 
   // Metodos  	
     public double convertirADolar(double cantidad) {
         return cantidad * tipoCambioDolar;
+        
     }
+    
+    public double convertirPesosDolar(double cantidad) {
+		return cantidad * tipoCambioPesoD; 
+	}
 
     public double convertirAEuro(double cantidad) {
         return cantidad * tipoCambioEuro;
+    }
+    public double convertirPesosEuro(double cantidad) {
+        return cantidad * tipoCambioPesoE;
     }
 
     public double convertirALibra(double cantidad) {
         return cantidad * tipoCambioLibra;
     }
+    
+    public double convertirPesosLibra(double cantidad) {
+        return cantidad * tipoCambioPesoL;
+    }
 
     public double convertirAYen(double cantidad) {
         return cantidad * tipoCambioYen;
     }
+    public double convertirPesosYen(double cantidad) {
+        return cantidad * tipoCambioPesoY;
+    }
 
     public double convertirAWon(double cantidad) {
         return cantidad * tipoCambioWon;
+    }
+    
+    public double convertirPesosW(double cantidad) {
+        return cantidad * tipoCambioPesoW;
     }
 }
 
--- src/Conversor_Monedas/Main.java
+++ src/Conversor_Monedas/Main.java
@@ -2,67 +2,130 @@ package Conversor_Monedas;
 
 import javax.swing.JOptionPane;
 
+import java.text.DecimalFormat;
+
+@SuppressWarnings("unused")
 public class Main {
     public static void main(String[] args) {
-        String[] opciones = { "Conversor de Moneda", "Conversor de Temperatura" };
-        String opcionSeleccionada = (String) JOptionPane.showInputDialog(null, "Seleccione una opción del Conversor",
-                "Convertidor", JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+        boolean ejecutar = true;
 
-        if (opcionSeleccionada.equals("Conversor de Moneda")) {
-            ConvertidorMoneda convertidorMoneda = new ConvertidorMoneda();
+        while (ejecutar) {
+            String[] opciones = { "Conversor de Moneda", "Conversor de Temperatura", "Salir" };
+            String opcionSeleccionada = (String) JOptionPane.showInputDialog(null,
+                    "Seleccione una opción", "Bienvenidos a Convertidor de Monedas", JOptionPane.QUESTION_MESSAGE, null,
+                    opciones, opciones[0]);
+            
+            
 
-            String[] opcionesMoneda = { "Dólar", "Euro", "Libra Esterlina", "Yen Japonés", "Won sur-coreano" };
-            String opcionMonedaSeleccionada = (String) JOptionPane.showInputDialog(null,
-                    "Seleccione la moneda de conversión", "Convertidor", JOptionPane.QUESTION_MESSAGE, null,
-                    opcionesMoneda, opcionesMoneda[0]);
-
-            String inputCantidad = JOptionPane.showInputDialog(null, "Ingresa la cantidad de moneda de tu país:");
-            double cantidad = Double.parseDouble(inputCantidad);
-
-            double resultadoMoneda = 0;
-
-            switch (opcionMonedaSeleccionada) {
-                case "Dólar":
-                    resultadoMoneda = convertidorMoneda.convertirADolar(cantidad);
-                    break;
-                case "Euro":
-                    resultadoMoneda = convertidorMoneda.convertirAEuro(cantidad);
-                    break;
-                case "Libra Esterlina":
-                    resultadoMoneda = convertidorMoneda.convertirALibra(cantidad);
-                    break;
-                case "Yen Japonés":
-                    resultadoMoneda = convertidorMoneda.convertirAYen(cantidad);
-                    break;
-                case "Won sur-coreano":
-                    resultadoMoneda = convertidorMoneda.convertirAWon(cantidad);
-                    break;
+            if (opcionSeleccionada.equals("Salir")) {
+                ejecutar = false;
+                continue;
             }
 
-            JOptionPane.showMessageDialog(null, "Resultado de la conversión: " + resultadoMoneda);
-        } else if (opcionSeleccionada.equals("Conversor de Temperatura")) {
-            ConvertidorTemperatura convertidorTemperatura = new ConvertidorTemperatura();
+            if (opcionSeleccionada.equals("Conversor de Moneda")) {
+                ConvertidorMoneda convertidorMoneda = new ConvertidorMoneda();
 
-            String[] opcionesTemperatura = { "Celsius", "Fahrenheit" };
-            String opcionTemperaturaSeleccionada = (String) JOptionPane.showInputDialog(null,
-                    "Seleccione la escala de temperatura", "Convertidor de Temperatura", JOptionPane.QUESTION_MESSAGE,
-                    null, opcionesTemperatura, opcionesTemperatura[0]);
+                String[] opcionesMoneda = { "Dólar a pesos", "Euro a pesos", "Libra Esterlina a pesos", "Yen Japonés a pesos", "Won sur-coreano a pesos",
+                		"De pesos a Dólar", "De pesos a Euro", "De pesos Libra Esterlinas", "De pesos a Yen Japonés", "De pesos a Won sur-coreano" };
+                String opcionMonedaSeleccionada = (String) JOptionPane.showInputDialog(null,
+                        "Seleccione la moneda de conversión", "Convertidor", JOptionPane.QUESTION_MESSAGE, null,
+                        opcionesMoneda, opcionesMoneda[0]);
 
-            String inputTemperatura = JOptionPane.showInputDialog(null, "Ingresa la temperatura:");
-            double temperatura = Double.parseDouble(inputTemperatura);
+                double cantidad = obtenerCantidad("Ingresa la cantidad de moneda a convertir:");
 
-            double resultadoTemperatura = 0;
+                if (cantidad == 0) {
+                    JOptionPane.showMessageDialog(null, "Ingrese un valor que no sea 0");
+                    continue;
+                }
 
-            switch (opcionTemperaturaSeleccionada) {
-                case "Celsius":
-                    resultadoTemperatura = convertidorTemperatura.convertirACelsius(temperatura);
-                    break;
-                case "Fahrenheit":
-                    resultadoTemperatura = convertidorTemperatura.convertirAFahrenheit(temperatura);
-                    break;
+                double resultadoMoneda = 0;
+
+                switch (opcionMonedaSeleccionada) {
+                    case "Dólar a pesos":
+                        resultadoMoneda = convertidorMoneda.convertirADolar(cantidad);
+                        break;
+                    case "Euro a pesos":
+                        resultadoMoneda = convertidorMoneda.convertirAEuro(cantidad);
+                        break;
+                    case "Libra Esterlina a pesos":
+                        resultadoMoneda = convertidorMoneda.convertirALibra(cantidad);
+                        break;
+                    case "Yen Japonés a pesos":
+                        resultadoMoneda = convertidorMoneda.convertirAYen(cantidad);
+                        break;
+                    case "Won sur-coreano a pesos":
+                        resultadoMoneda = convertidorMoneda.convertirAWon(cantidad);
+                        break;
+                        
+                   case "De pesos a Dólar":
+                       resultadoMoneda = convertidorMoneda.convertirPesosDolar(cantidad);
+                       break;
+                   case "De pesos a Euro":
+                       resultadoMoneda = convertidorMoneda.convertirPesosEuro(cantidad);
+                       break;
+                   case "De pesos a Libra Esterlina":
+                       resultadoMoneda = convertidorMoneda.convertirPesosLibra(cantidad);
+                       break;
+                   case "De pesos a Yen Japonés":
+                       resultadoMoneda = convertidorMoneda.convertirPesosYen(cantidad);
+                       break;
+                   case "De pesos Won sur-coreano":
+                       resultadoMoneda = convertidorMoneda.convertirPesosW(cantidad);
+                       break;     
+                        
+                }
+   
+
+                JOptionPane.showMessageDialog(null, "Resultado de la conversión: " + resultadoMoneda);
+                
+                                          
+                int opcionConfirmacion = JOptionPane.showConfirmDialog(null, "Desea Continuar?", "Confirmación", JOptionPane.YES_NO_OPTION);
+
+                if (opcionConfirmacion == JOptionPane.NO_OPTION) {
+                    ejecutar = false;
+                }
+              
+                
+                
+
+            } else if (opcionSeleccionada.equals("Conversor de Temperatura")) {
+                ConvertidorTemperatura convertidorTemperatura = new ConvertidorTemperatura();
+
+                String[] opcionesTemperatura = { "Celsius", "Fahrenheit" };
+                String opcionTemperaturaSeleccionada = (String) JOptionPane.showInputDialog(null,
+                        "Seleccione la escala de temperatura", "Convertidor de Temperatura",
+                        JOptionPane.QUESTION_MESSAGE, null, opcionesTemperatura, opcionesTemperatura[0]);
+
+                double temperatura = obtenerCantidad("Ingresa la temperatura:");
+
+                double resultadoTemperatura = 0;
+
+                switch (opcionTemperaturaSeleccionada) {
+                    case "Celsius":
+                        resultadoTemperatura = convertidorTemperatura.convertirACelsius(temperatura);
+                        break;
+                    case "Fahrenheit":
+                        resultadoTemperatura = convertidorTemperatura.convertirAFahrenheit(temperatura);
+                        break;
+                }
+
+                JOptionPane.showMessageDialog(null, "Resultado de la conversión: " + resultadoTemperatura);
+                
             }
+        }
+    }
+  
 
-            JOptionPane.showMessageDialog(null, "Resultado de la conversión: " + resultadoTemperatura);
+    private static double obtenerCantidad(String mensaje) {
+        while (true) {
+            String inputCantidad = JOptionPane.showInputDialog(null, mensaje);
+
+            try {
+                double cantidad = Double.parseDouble(inputCantidad);
+                return cantidad;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Ingrese un número válido");
+            }
         }
     }
 }
